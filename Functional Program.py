8f59a2924eee224b9020fb9145850f56909a6cbf
MaryamJamali  ❤️ <150019136+MaryaJamali@users.noreply.github.com>
2024-04-10 20:33:21 +0330

    Add programmer and developer profile

--- Functional Program.py
+++ Functional Program.py
@@ -39,3 +39,7 @@ def text_analysis():
 
 
 print('Number of words in your text: ', text_analysis())
+
+# Name of the programmer: Maryam Jamali
+# Email address: m.jamali16@yahoo.com
+# GitHub address: https://github.com/MaryaJamali
